b820d12510f6e9f3b84bcd656445bb4e7c843f67
trujilml <mickeytrj@gmail.com>
2021-10-18 16:33:20 -0500

    catch data returned from github api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    });
+    });
+
+    console.log("outside");
+    var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
 };
 
 getUserRepos();
