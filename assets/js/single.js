6f537231cc12151448e8efce48dcdb7d72d30e45
trujilml <mickeytrj@gmail.com>
2021-10-20 12:33:12 -0500

    used query parameter to identify specific repo issues from github generated link

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,14 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+
+var getRepoName = function() {
+    var queryString = document.location.search; 
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent(repoName);
+}
+
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -74,4 +82,4 @@ var getRepoIssues = function(repo) {
      limitWarningEl.appendChild(linkEl);
  };
 
-getRepoIssues("facebook/react");
+getRepoName();
