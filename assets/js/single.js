6c43605eeaa5c8326a6f71cd2150496995c2caf5
trujilml <mickeytrj@gmail.com>
2021-10-20 01:03:53 -0500

    added message if more than 30 issues are listed

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,15 +1,24 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    console.log(displayWarning(repo));
+                }
             });
         }
         else {
+            console.log(response)
             alert("There was a problem with your request!");
         }
     });
@@ -48,8 +57,21 @@ var getRepoIssues = function(repo) {
         //append to container
         issueEl.appendChild(typeEl);
 
+        //append to dom
         issueContainerEl.appendChild(issueEl);
     }
+ };
+
+ var displayWarning = function(repo) {
+     limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+     var linkEl = document.createElement("a");
+     linkEl.textContent = "GitHub.com";
+     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+     linkEl.setAttribute("target", "_blank");
+
+     //append to warning container
+     limitWarningEl.appendChild(linkEl);
  };
 
 getRepoIssues("facebook/react");
